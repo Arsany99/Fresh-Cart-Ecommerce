638fa508c77904811b449b6145085e9a212c0953
Arsany <arsanyraafat@gmail.com>
2024-05-09 13:52:16 +0300

    updated

--- src/components/CategoriesSlider/CategoriesSlider.jsx
+++ src/components/CategoriesSlider/CategoriesSlider.jsx
@@ -30,7 +30,7 @@ export default function CategoriesSlider() {
 }
   return (
     <Slider {...settings}>
-    {data.data.data.map((category , idx)=> <div className="row" key={idx} >
+    {data.data.data.map((category , idx)=> <div key={idx} >
        <img style={{height:"200px"}} className=" w-100" src={category.image} alt={category.name} />
        <h4 className="overflow-hidden m-3">{category.name}</h4>
     </div> )}
