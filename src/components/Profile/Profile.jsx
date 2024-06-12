f2b5a52bbe4e195aeb4336c93546629448c71cc0
Arsany <arsanyraafat@gmail.com>
2024-06-12 10:13:14 +0300

    last design

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,50 +1,34 @@
-import React, { useContext } from 'react'
-import authContext from '../../Context/AuthContext.js'
-import imageProfile from '../../images/ava3.webp'
-import { cartContext } from '../../Context/CartContext.js'
-
+import React, { useContext } from 'react';
+import authContext from '../../Context/AuthContext.js';
+import imageProfile from '../../images/ava3.webp';
+import { cartContext } from '../../Context/CartContext.js';
 
 function Profile() {
+    const { userData } = useContext(authContext);
+    const { numOfCartItems } = useContext(cartContext);
+    const { totalCartPrice } = useContext(cartContext);
 
-
-    const { userData } = useContext(authContext)
-    const{numOfCartItems}= useContext(cartContext)
-    const{totalCartPrice}= useContext(cartContext)
-
-
-    return <>
-    
-
-    <div className="container p-5">
-        <div className="w-100 m-5 p-5 text-center shadow bg-body-secondary">
-            <h1 className='text-main fst-italic'>Details For your Account</h1>
-            <div className="row h-75">
-                <div className="col-4">
-                        <div className="w-100">
-                            <img src={imageProfile} alt="imageProfile" className='w-100' />
-                        </div>  
-                </div>
-                <div className="col-8">
-                    <div className="w-100 p-5">
-                        <h1 className='text-main fw-bolder'>Hello {userData?.name}</h1>
-                        <h5 className='fw-bolder'>Number of Product : {numOfCartItems} item</h5>
-                        <h5 className='fw-bolder'>Total Card Price : {totalCartPrice}$</h5>
+    return (
+        <div className="container mb-5 p-5">
+            <div className="m-auto mt-5 p-5 text-center shadow bg-body-secondary">
+                <h1 className="text-main fst-italic">Details For your Account</h1>
+                <div className="row ">
+                    <div className="col-md-4">
+                        <div className="w-75 m-auto">
+                            <img src={imageProfile} alt="Profile" className="img-fluid rounded-circle" />
+                        </div>
                     </div>
-
+                    <div className="col-md-8 ">
+                        <div className="w-100 p-5">
+                            <h2 className="text-main d-inline text-nowrap fw-bolder">Hello {userData?.name}</h2>
+                            <p className=" fw-bolder text-nowrap mt-3">Number of Products: {numOfCartItems} item</p>
+                            <p className="fw-bolder text-nowrap ">Total Cart Price: {totalCartPrice}$</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-
-
-
         </div>
-    </div>
-    
-    
-    
-    </>
-        
-    
+    );
 }
 
-export default Profile
+export default Profile;
